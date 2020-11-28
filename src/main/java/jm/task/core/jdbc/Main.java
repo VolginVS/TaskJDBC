0e20470c66803c43c99a8f8d5867efb280edd248
Vsevolod Volgin <volginvs@list.ru>
2020-11-28 17:21:37 +0300

    Task 1.1.4: implement methods using Hibernate (1.1)

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -39,6 +39,7 @@ public class Main {
         }
         System.out.println("-----");
         userService.cleanUsersTable();
+        userService.dropUsersTable();
         Util.shutdown();
     }
 }
