919c460588ef7d54086da8dadbda01df5d5af0db
Vsevolod Volgin <volginvs@list.ru>
2020-12-01 11:29:05 +0300

    fully java configuration without XML

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,11 +1,20 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 public class Util {
     private static final String USERNAME = "root";
@@ -25,14 +34,27 @@ public class Util {
         return connection;
     }
 
+
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
-            try {
-                sessionFactory = new Configuration().configure().buildSessionFactory();
-            } catch (Exception e) {
-                e.printStackTrace();
-                throw new ExceptionInInitializerError(e);
-            }
+            Properties properties = new Properties();
+            properties.put("connection.driver_class", "com.mysql.jdbc.Driver");
+            properties.put("dialect", "org.hibernate.dialect.MySQL8Dialect");
+            properties.put("hibernate.connection.url",
+                            "jdbc:mysql://localhost:3306/jm_db?serverTimezone=UTC");
+            properties.put("hibernate.connection.username", "root");
+            properties.put("hibernate.connection.password", "root");
+            properties.put("hibernate.current_session_context_class", "thread");
+            properties.put("hibernate.show_sql", "true");
+            properties.put("hibernate.format_sql", "true");
+
+            Configuration configuration = new Configuration();
+            configuration.setProperties(properties);
+            configuration.addAnnotatedClass(User.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
         }
         return sessionFactory;
     }
@@ -41,3 +63,6 @@ public class Util {
         getSessionFactory().close();
     }
 }
+
+
+
